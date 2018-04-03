6a74b073b1adc7491cb3eda32c703331d4f9cf5b
WarSocialist <37631867+WarSocialist@users.noreply.github.com>
2018-04-02 21:59:12 -0500

    totallyCommiting

--- stillLearningGit/stillLearningGit/main.cpp
+++ stillLearningGit/stillLearningGit/main.cpp
@@ -4,6 +4,7 @@ int main()
 {
 	std::cout << "Hello World" << std::endl;
 	std::cout << "I have no idea if this will work but hey let us check" << std::endl;
+	//I Just wana commment stuff and have it update
 	return 0;
 
 }
